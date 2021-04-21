234d0c48a8d6e10fcf2dc03ca03a1a429c88aa66
Jonas Henrique Nascimento <jonashenrique989@gmail.com>
2021-04-21 15:58:11 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,118 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native'
+
+import * as firebase from 'firebase'
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+
+/*
+ * Tutorial para configurar firebase
+ * 
+ * 1º Criar projeto no firebase com o nome de "instagram-dev", manter google analytics ativado
+ * 2º Ir na aba Authentication | sign in method | ativar opçao por email
+ * 3º Ir na aba Cloud Firestore| Criar o banco de dados do firestore em modo de testes
+ * 4º Ir na aba Project Overview e depois na opção "Web", 
+ * 4.1 Register App colocar "instagram-dev", concluir
+ * 4.2 Ir na opção das engrenagens do lado da aba 'Project Overview'| Primeira opção, project config
+ * 4.3 No final da pagina copiar a parte da variável 'firebaseConfig' e substituir pela a que está em baixo
+ */
+
+
+const firebaseConfig = {
+  apiKey: "??????????",
+  authDomain: "??????????",
+  projectId: "??????????",
+  storageBucket: "??????????",
+  messagingSenderId: "??????????",
+  appId: "??????????",
+  measurementId: "??????????",
+};
+
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+import LoginScreen from './components/auth/Login'
+import MainScreen from './components/Main'
+import AddScreen from './components/main/Add'
+import SaveScreen from './components/main/Save'
+import CommentScreen from './components/main/Comment'
+
+
+const Stack = createStackNavigator();
+
+
+export class App extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+
+    return (
+      <Provider store={store}>
+        <NavigationContainer >
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={MainScreen} />
+            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation}/>
+            <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation}/>
+            <Stack.Screen name="Comment" component={CommentScreen} navigation={this.props.navigation}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    )
+  }
+}
+
+export default App
